bb55f4f5354c0c20e851c4593241ae3cdbb0ef6c
DayanaM <dmmartinez269@misena.edu.co>
2021-02-02 11:33:09 -0500

    Generate components panel and subject

--- university-subjects/src/app/app.module.ts
+++ university-subjects/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanelComponent } from './components/panel/panel.component';
+import { SubjectComponent } from './components/subject/subject.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    SubjectComponent
   ],
   imports: [
     BrowserModule,
